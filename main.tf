87347d09ea2ef99716964bf4bf64ea293a5dd48c
madebydawid <dawid.kuzbiel@gmail.com>
2024-04-29 15:50:59 +0200

    added main.tf code

--- main.tf
+++ main.tf
@@ -1,26 +1,41 @@
-# 1. We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
+# Fetches the configuration for current Azure client
+data "azurerm_client_config" "current" {}
+
+# 1. Create a resource group
+resource "azurerm_resource_group" "arc-rg" {
+  name     = "azureresume-rg"
+  location = var.resource_group_location
+}
+
+# 2. Create a resource account
+resource "azurerm_storage_account" "arc-storage" {
+  name                            = "dawidarcstorage"
+  resource_group_name             = azurerm_resource_group.arc-rg.name
+  location                        = azurerm_resource_group.arc-rg.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+  enable_https_traffic_only       = true
+  allow_nested_items_to_be_public = true
+
+  static_website {
+    index_document = "index.html"
   }
 }
 
-# 2. Configure the AzureRM Provider
-provider "azurerm" {        # The AzureRM Provider supports authenticating using via the Azure CLI
-    features {              # The features block allows changing the behaviour of the Azure Provider
-      
-    }
-  
+# 3. Create a resource container
+resource "azurerm_storage_container" "static-page" {
+  name = "static-web-page"
+  storage_account_name  = azurerm_storage_account.arc-storage.name
+  container_access_type = "private"
 }
 
-# 3. Create a resource group
-resource "azurerm_resource_group" "mrc-rg" {
-  name     = "myresumechallenge-rg"
-  location = "North Europe"
+# 4. Create a blob
+resource "azurerm_storage_blob" "html-code" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.arc-storage.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
 }
-
-#. Create Storage Account for
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "resource_group_location" {
+    type = string
+    description = "Location of the resource group"
+    default = "northeurope"
+  
+}
